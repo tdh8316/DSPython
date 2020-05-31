60438182db3e16d8c2064711a9cfd65a657e9d42
탁동혁 <tdh8316@naver.com>
2020-05-31 10:08:42 +0900

    core: Fix mismatched type

--- include/Serial.h
+++ include/Serial.h
@@ -3,6 +3,6 @@
 #include <HardwareSerial.h>
 
 extern "C" void print__i__(int n);
-extern "C" void print__f__(int n);
+extern "C" void print__f__(float n);
 extern "C" void print__s__(char c[]);
 extern "C" void begin(int b);
