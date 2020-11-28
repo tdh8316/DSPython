1edcf4a3e51262694190fe45be2df0e484570dba
탁동혁 <tdh8316@naver.com>
2020-11-28 21:36:29 +0900

    main: Update command-line arguments

--- src/bin/dspython.rs
+++ src/bin/dspython.rs
@@ -15,8 +15,8 @@ fn parse_arguments<'a>(app: App<'a, '_>) -> ArgMatches<'a> {
         .required(true)
         .help("The source file");
     let arg_port = Arg::with_name("port")
-        .help("Serial port to upload your work")
-        .long("upload")
+        .help("Serial port of an Arduino to upload")
+        .long("--upload-to")
         .short("u")
         .takes_value(true);
     let arg_opt = Arg::with_name("opt_level")
@@ -25,27 +25,19 @@ fn parse_arguments<'a>(app: App<'a, '_>) -> ArgMatches<'a> {
         .short("o")
         .takes_value(true);
     let arg_remove_hex = Arg::with_name("remove_hex")
-        .help("Remove the hex file after finishing compilation")
+        .help("Remove generated hex file")
         .long("--remove-hex")
         .takes_value(false);
     let arg_emit_llvm = Arg::with_name("emit_llvm")
         .help("Emit LLVM IR")
         .long("--emit-llvm")
         .takes_value(false);
-    let arg_include_libs = Arg::with_name("include_libs")
-        .help("Manually specific core libraries to include")
-        .long("--include-libs")
-        .short("I")
-        .takes_value(true)
-        .multiple(true)
-        .required(false);
 
     app.arg(arg_file)
         .arg(arg_opt)
         .arg(arg_port)
         .arg(arg_remove_hex)
         .arg(arg_emit_llvm)
-        .arg(arg_include_libs)
         .get_matches()
 }
 
@@ -53,25 +45,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     let app = App::new("dspython")
         .version(VERSION)
         .author(AUTHORS)
-        .about("DSPython is a damn small Python compiler for Arduino.");
+        .about("DSPython is a damn small Python compiler intended to use in Arduino.");
 
     let matches = parse_arguments(app);
 
-    let file = matches.value_of("file").unwrap();
+    let file = matches.value_of("file").expect("no input files");
     let port = matches.value_of("port");
 
-    let optimization_level = matches.value_of("opt_level").unwrap_or("3").parse::<u8>()?;
-    let include_libs = if let Some(libs) = matches.values_of("include_libs") {
-        libs.collect::<Vec<&str>>()
-    } else {
-        vec![
-            "arduino/constants.py",
-            "arduino/uno_pins.py",
-            "arduino/math.py",
-        ]
-    };
+    let optimization_level = matches.value_of("opt_level").unwrap_or("2").parse::<u8>()?;
 
-    let compiler_flags = CompilerFlags::new(optimization_level, include_libs);
+    let compiler_flags = CompilerFlags::new(optimization_level);
 
     let assembly = match get_assembly(file.to_string(), compiler_flags) {
         Ok(llvm_string) => llvm_string,
@@ -88,11 +71,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         let hex_file = File::open(&hex)?;
         let file_size = hex_file.metadata().unwrap().len();
 
+        /*
         println!(
             "The result is written to {} ({}KB of 30KB)",
             &hex,
             file_size / 1024
         );
+        */
 
         if file_size > 30 * 1024 {
             println!(
