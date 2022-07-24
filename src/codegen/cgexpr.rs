cae65abc2cf86e7997178b9e724bec98cac92407
Donghyeok Tak <tdh8316@naver.com>
2022-07-24 18:50:33 +0900

    Add the add operator for int and float

--- src/codegen/cgexpr.rs
+++ src/codegen/cgexpr.rs
@@ -2,7 +2,7 @@ use rustpython_parser::ast;
 
 use crate::codegen::errors::CodeGenError;
 use crate::codegen::symbol_table::SymbolValueTrait;
-use crate::codegen::value::{truncate_bigint_to_u64, Value};
+use crate::codegen::value::{truncate_bigint_to_u64, Value, ValueType};
 use crate::codegen::CodeGen;
 
 impl<'a, 'ctx> CodeGen<'a, 'ctx> {
@@ -12,9 +12,51 @@ impl<'a, 'ctx> CodeGen<'a, 'ctx> {
         match &expr.node {
             Constant { value, kind } => self.emit_constant(value, kind),
             Name { id, .. } => self.emit_name(id),
+            BinOp { left, op, right } => self.emit_bin_op(left, op, right),
 
-            _ => Err(CodeGenError::Unimplemented(format!("expr: {:?}", expr))),
+            _ => Err(CodeGenError::Unimplemented(format!("expr: {:#?}", expr))),
         }
+    }
+
+    fn emit_bin_op(&mut self, left: &ast::Expr, op: &ast::Operator, right: &ast::Expr) -> Result<Value<'ctx>, CodeGenError> {
+        let result_value = match op {
+            ast::Operator::Add => self.emit_add(left, right)?,
+            _ => return Err(CodeGenError::Unimplemented(format!("operator: {:#?}", op))),
+        };
+        Ok(result_value)
+    }
+
+    fn emit_add(&mut self, left: &ast::Expr, right: &ast::Expr) -> Result<Value<'ctx>, CodeGenError> {
+        let left_value = self.emit_expr(left)?;
+        let right_value = self.emit_expr(right)?;
+
+        let result_value = match (left_value.get_type(), right_value.get_type()) {
+            (ValueType::I32, ValueType::I32) => {
+                let value = self.builder.build_int_add(left_value.to_basic_value().into_int_value(), right_value.to_basic_value().into_int_value(), "add");
+                Value::I32 { value }
+            }
+            (ValueType::F32, ValueType::F32) => {
+                let value = self.builder.build_float_add(left_value.to_basic_value().into_float_value(), right_value.to_basic_value().into_float_value(), "add");
+                Value::F32 { value }
+            }
+            (ValueType::I32, ValueType::F32) => {
+                let left_value = self.builder.build_signed_int_to_float(left_value.to_basic_value().into_int_value(), self.context.f32_type(), "i32_to_f32");
+                let value = self.builder.build_float_add(left_value, right_value.to_basic_value().into_float_value(), "add");
+                Value::F32 { value }
+            }
+            (ValueType::F32, ValueType::I32) => {
+                let right_value = self.builder.build_signed_int_to_float(left_value.to_basic_value().into_int_value(), self.context.f32_type(), "i32_to_f32");
+                let value = self.builder.build_float_add(left_value.to_basic_value().into_float_value(), right_value, "add");
+                Value::F32 { value }
+            }
+            _ => return Err(
+                CodeGenError::TypeError(
+                    format!("{:?}", left_value.get_type()), format!("{:?}", right_value.get_type()),
+                )
+            ),
+        };
+
+        Ok(result_value)
     }
 
     fn emit_name(&mut self, id: &String) -> Result<Value<'ctx>, CodeGenError> {
