16f9cb5a9b46c2ebbe1b830b1a3d4ed6c951d51f
Donghyeok Tak <tdh8316@naver.com>
2022-08-14 00:07:50 +0900

    Add a core stdin library

--- python/include/python_stdio.h
+++ python/include/python_stdio.h
@@ -4,6 +4,9 @@
 void print_v(void);
 void print_i(int value);
 void print_f(float value);
-void print_p(char value[]);
+void print_p(char *value);
+
+char *input_v(void);
+char *input_p(char *value);
 
 #endif // PYTHON_STDIO
--- python/src/python_stdio.c
+++ python/src/python_stdio.c
@@ -1,17 +1,47 @@
 #include <stdio.h>
 
-void print_v() {
+static char inputBuffer[0xFF + 1];
+
+void print_v(void)
+{
     printf("\n");
 }
 
-void print_i(int value) {
+void print_i(int value)
+{
     printf("%d\n", value);
 }
 
-void print_f(float value) {
+void print_f(float value)
+{
     printf("%f\n", value);
 }
 
-void print_p(char value[]) {
+void print_p(char *value)
+{
     printf("%s\n", value);
+}
+
+char *input_v(void)
+{
+    fgets(inputBuffer, sizeof(inputBuffer), stdin);
+
+    // remove the trailing newline
+    inputBuffer[strcspn(inputBuffer, "\n")] = 0;
+    inputBuffer[strcspn(inputBuffer, "\r\n")] = 0;
+
+    return inputBuffer;
+}
+
+char *input_p(char *value)
+{
+    printf("%s", value);
+    fflush(stdout);
+    fgets(inputBuffer, sizeof(inputBuffer), stdin);
+
+    // remove the trailing newline
+    inputBuffer[strcspn(inputBuffer, "\n")] = 0;
+    inputBuffer[strcspn(inputBuffer, "\r\n")] = 0;
+
+    return inputBuffer;
 }
