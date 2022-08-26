a637deac37a725081aafe8ff26405d068137c2fb
Donghyeok Tak <tdh8316@naver.com>
2022-08-26 14:00:03 +0900

    Implement behavior for int when invalid literal passed

--- python/src/python_builtins.c
+++ python/src/python_builtins.c
@@ -1,10 +1,22 @@
+#include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 static int n;
 
 int int_s(const char *value)
 {
+    const char *chars = strdup(value);
+    while (*chars)
+    {
+        if (!isdigit(*chars++))
+        {
+            printf("ValueError: Invalid literal for int() with base 10: '%s'\n", value);
+            exit(1);
+        }
+    }
+
     n = atoi(value);
     return n;
 }
